2c04c0b57349b4f95e6215765800a8c5adaebb68
John Yam <mrjohnyam@gmail.com>
2014-04-02 13:44:34 -0400

    test2

---
+++ testUITableView/testUITableView/JLYtestTableView.h
@@ -0,0 +1,14 @@
+//
+//  JLYtestTableView.h
+//  testUITableView
+//
+//  Created by JOHN YAM on 4/1/14.
+//  Copyright (c) 2014 John Yam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JLYtestTableView : UITableView
+
+
+@end
---
+++ testUITableView/testUITableView/JLYtestTableView.m
@@ -0,0 +1,34 @@
+//
+//  JLYtestTableView.m
+//  testUITableView
+//
+//  Created by JOHN YAM on 4/1/14.
+//  Copyright (c) 2014 John Yam. All rights reserved.
+//
+
+#import "JLYtestTableView.h"
+
+@implementation JLYtestTableView
+
+- (id)initWithFrame:(CGRect)frame style:(UITableViewStyle)style
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+        self.backgroundColor = [UIColor darkGrayColor];
+        
+    }
+    return self;
+}
+
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
